f5f37a127f9fc4d68d6470671c654294b0f0125d
stojanstolfa <stojan.stolfa@gmail.com>
2012-10-09 10:56:23 +0200

    testiramo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Vrni naslov glede na vsebino strani
+  def title
+    base_title = "RoR Tutorial sample app"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}  "
+    end
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                      :content => "Ruby on R sample app | Home")
     end
   end
 
@@ -16,4 +23,10 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
